cced02bed540d578c7e502ddc1bb4c79643cca81
frannie <frances.ziesemer@gmail.com>
2021-03-24 13:08:26 +0100

    convert to hooks - input problem remains

--- src/App.js
+++ src/App.js
@@ -1,98 +1,45 @@
-import React from 'react';
-import './App.css';
-import AppWrapper from './components/app-wrapper/app-wrapper.component';
-import SubmitForm from './components/submit-form/submit-form.component';
-import TodoList from './components/todo-list/todo-list.component';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import AppWrapper from "./components/app-wrapper/app-wrapper.component";
+import SubmitForm from "./components/submit-form/submit-form.component";
+import TodoList from "./components/todo-list/todo-list.component";
 
+function App() {
+  const [tasks, setTasks] = useState([]);
 
-class App extends React.Component{
-  //state objects here 
-  state = {
-    tasks: [],
-    //maybe need a loading state here:
-    //isLoading is to inform the user that the site is fetching data -
-    // it may take a few seconds
-
-  }
-
-  //functions here
-
-
-  handleDelete = (index) => {
+  const handleDelete = (index) => {
     //create a copy of the current tasks array
-    const newArr = [...this.state.tasks];
-    //find the id of the task via index that was passed into this function
-    const id = newArr[index].id;
-    console.log(id);
-    //here we have to add the id to the end of the url because we need to target the correct api object
-    fetch("https://frauenloop-todo-service.herokuapp.com/api/todos/" + id, {
-      method: 'DELETE'
-    })
-    //why is this text???
-      .then(response => response.text())
-      .then(todos => {
-        //remove the item from index 
-        newArr.splice(index, 1);
-        //re - setState as new array
-        this.setState({ tasks: newArr });
-      });
-  }
-
-  //this function ultimately takes the input data that we have saved in the state object (title and content ) 
-  // then it adds it to the api !!
-  handleAdd = task => {
-    fetch("https://frauenloop-todo-service.herokuapp.com/api/todos", {
-      //Post is the instruction to add to api 
-      method: 'POST',
-      //look this up??? 
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      // i think this part turns the data into the correct json format
-      body: JSON.stringify(task)
-    })
-      .then(response => response.json())
-      .then(task => {
-        //takes input  from submit-form component and adds it to json api data
-        this.setState({ tasks: [...this.state.tasks, task] });
-      });
+    const newArr = [...tasks];
+    newArr.splice(index, 1);
+    setTasks(newArr);
   };
 
+  const handleSubmit = (task) => {
+    setTasks([...tasks, task]);
+  };
 
-
-  // this function does the first step of api call 
-  //cDM() executes a second render (1st render()) of the page with the data fetched from api
-  //after cDM() function api date is stored locally and can be displayed or passed to other functions as props
-  //cDM means that component is rendered
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://frauenloop-todo-service.herokuapp.com/api/todos")
-      //pulls the response from url into json file
-      .then(response => response.json())
-      //updates our state object to = the json file 
-      //(todos refers to complete data inside api)
-      .then(todos => {
-        this.setState({ tasks: todos });
-      });
-  }
+      .then((response) => response.json())
+      .then((todos) => setTasks(todos));
+  }, []);
 
-  render() {
   return (
-   //JSX here
-      <AppWrapper> 
-        <div className="column  m-4"> 
-          <div className="row box">     
-              {/* here we have to send the handleAdd function downwards using props called handleAdd. */}
-              <SubmitForm handleAdd={this.handleAdd} />
+    <div className="App">
+      <AppWrapper>
+        <div className="column  m-4">
+          <div className="row box">
+            {/* here we have to send the handleAdd function downwards using props called handleAdd. */}
+            <SubmitForm onAdd={handleSubmit} />
+          </div>
+          <div className="row"> </div>
         </div>
-        <div className="row"> </div>
+        <div className="column box mt-5 mr-6">
+          <TodoList tasks={tasks} onDelete={handleDelete} />
         </div>
-        <div className="column box mt-5 mr-6"> 
-          <TodoList tasks={this.state.tasks} onDelete={this.handleDelete} />
-        </div>
-      </AppWrapper>  
-  
+      </AppWrapper>
+    </div>
   );
-  }
 }
 
 export default App;
--- src/components/submit-form/submit-form.component.jsx
+++ src/components/submit-form/submit-form.component.jsx
@@ -1,41 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './submit-form.css';
 
 // we need to use a class component because we have state information/ updating inside the component 
-class SubmitForm extends React.Component {
-  //state object, input is undefined because we want the input clear field empty on load
-  state = {
-    title: '',
-    content: '',
+function SubmitForm (props) {
+
+  //state object, is undefined because we want the fields empty on load
+  const [task, setTask] = useState({ title: "", content: ""});
+
+  const handleInputChange = (event) => {
+    const {name, value} = event.target;
+    setTask(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+    console.log(event.target)
   }
 
   //onSubmit function is an event called in the <form> tag and called when submit button is clicked 
-  onSubmit = (event) => {
-    event.preventDefault();
-    //onSubmit function calls handle submit function from App.js component
+  //onSubmit function calls handle submit function from App.js component
     //handleSubmit function has been sent down to this component via props
     //it passes in the input value 
-    this.props.handleAdd(this.state);
     //sets state back to empty for the input field on submit
-    this.setState({ title: '', content: '', });
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onAdd(task);
+    console.log(task);
+    setTask({ title: "", content: ""});
   }
 
-
-  render () {
   return (
     <div className="submit-form">
-      <form className="field" onSubmit={this.onSubmit}>
+      <form className="field" onSubmit={handleSubmit}>
         <div className="control">
           <label>Add new:</label>
           <input 
             className="input mr-4 mt-2" 
             type="text" 
             placeholder="Add new list item here..." 
-            value = {this.state.title}
+            value = {task.title}
             //onChange to handle input value 
             // updates state (title) object within this component
-            onChange={(event) => this.setState({title: event.target.value})}
+            onChange={handleInputChange}
           />
           
          
@@ -43,8 +49,8 @@ class SubmitForm extends React.Component {
         <div className="">
           <textarea 
             placeholder="Content" 
-            value={this.state.content} 
-            onChange={(event) => this.setState({content: event.target.value})}
+            value={task.content} 
+            onChange={handleInputChange}
             className="textarea mt-2 mb-2"
           ></textarea>
           
@@ -54,13 +60,11 @@ class SubmitForm extends React.Component {
           <button 
             className="button is-info" 
             type="reset"
-            onClick={(event) => this.setState({title: '', content: '' })}
           >Reset</button>
         </div>
       </form>
     </div>
   )
-  }
 }
 
 export default SubmitForm;
